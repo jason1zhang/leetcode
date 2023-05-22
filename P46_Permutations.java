e7f02c9cf24e4052902ab51e157354ed58b47efd
jason1zhang <jason1_zhang@hotmail.com>
2023-05-22 10:09:25 +0800

    Update on P46_Permutations

---
+++ P46_Permutations.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+/**
+ * solutions to leetcode problem 46: Permutations
+ * 
+ * @author Jason Zhang
+ * @version 1.0
+ * @since 2022-06-03
+ */
+
+ public class P46_Permutations {
+    public static void main(String[] args) {
+        P46_Permutations p46_Permutations = new P46_Permutations();
+
+        p46_Permutations.test();
+    }
+
+    public void test() {
+        // test case 1        
+        int[] nums = {1, 2, 3};
+
+        System.out.printf("The permutations.\n");
+
+    }
+
+     /**
+      * Problem #:      46
+      * Problem:        Permutations
+      * Difficulty:     Medium
+      * 
+      * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+      * 
+      * Example 1:
+      * Input: nums = [1,2,3]
+      * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+      * 
+      * Constraints:
+      *     - 1 <= nums.length <= 6
+      *     - -10 <= nums[i] <= 10
+      *     - All the integers of nums are unique.
+      */  
+
+    /**
+     * 
+     * Solution 1: 
+     * 
+     * Result:
+     *    - Runtime: 
+     * 
+     *    - time complexity:    O()
+     *    - space complexity:   O()
+     * 
+     * @param nums an array of distinct integers 
+     * @return all the possible permutations
+     */
+    public static List<List<Integer>> permute(int[] nums) {
+
+    }
+
+}
